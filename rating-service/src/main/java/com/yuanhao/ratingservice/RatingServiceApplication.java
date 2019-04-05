b9a2376f610147a33a5c12b33df5335f45295bd0
yuanhao <guyuanhao@foxmail.com>
2019-04-05 23:55:45 +0800

    add book-service & rating-sevice

--- rating-service/src/main/java/com/yuanhao/ratingservice/RatingServiceApplication.java
+++ rating-service/src/main/java/com/yuanhao/ratingservice/RatingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.yuanhao.bookservice;
+package com.yuanhao.ratingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class BookServiceApplication {
+public class RatingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookServiceApplication.class, args);
+        SpringApplication.run(RatingServiceApplication.class, args);
     }
 
 }
